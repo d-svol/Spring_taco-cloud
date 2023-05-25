6bddc37054e3dbfe976c67dd2a33be38a3cb61d8
Denis <volynshykovdenis@gmail.com>
2023-05-25 13:09:18 +0300

    d-svol
    delete old files

--- src/main/java/net/dsvol/controller/HomeController.java
+++
@@ -1,9 +0,0 @@
-package net.dsvol.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-}
